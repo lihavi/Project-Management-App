b018d2dd4a0d5f5e8679f20f5fe8abbc02ddc956
vivianne <viviannjoroge91@gmail.com>
2023-02-28 15:08:17 +0300

    add components

--- Frontend/project-management/src/App.js
+++ Frontend/project-management/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/projects" component={Projects}/>
+      </div>
+    </BrowserRouter>
   );
 }
+
+const Home = () => {
+  return(
+    <div>
+      <h1>Welcome to Project Management App!</h1>
+
+    </div>
+  )
+}
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+    </div>
+  );
+};
 
 export default App;
