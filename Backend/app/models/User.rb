c1f831c5fc5acd7b193754c05ecd031239f76d5f
vivianne <viviannjoroge91@gmail.com>
2023-03-01 15:03:41 +0300

    update models

--- Backend/app/models/User.rb
+++ Backend/app/models/User.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_many :projects
-   # validates :username, presence: true, uniqueness: true
-   # validates :password_digest, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
 end
